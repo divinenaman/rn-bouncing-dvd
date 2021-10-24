1eb037412f421452e6a6363164d05fcb8f73eab0
Naman Agarwal <namanddev7@gmail.com>
2021-10-24 20:35:25 +0530

    animation working but goes out-of bound

--- App.js
+++ App.js
@@ -1,108 +1,63 @@
-import React, { useRef, useEffect } from "react";
-import { StyleSheet, Text, View, Animated, Dimensions } from "react-native";
+import React, { useRef, useEffect, useState } from "react";
+import { StyleSheet, Text, View, Animated, Dimensions, Easing } from "react-native";
+import DvdLogo from "./assets/dvdlogo";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   const IMAGE_WIDTH = 52;
   const IMAGE_HEIGHT = 52;
-  const SCREEN_DIMENSIONS = Dimensions.get("screen");
+  const SCREEN_DIMENSIONS = Dimensions.get("window");
+
   const BOUND_X = SCREEN_DIMENSIONS.width - IMAGE_WIDTH;
-  const BOUND_Y = SCREEN_DIMENSIONS.height - IMAGE_HEIGHT;
+  const BOUND_Y = SCREEN_DIMENSIONS.height - IMAGE_HEIGHT - StatusBar.length;
 
-  const directionX = useRef(new Animated.Value(1)).current; 
-  const directionY = useRef(new Animated.Value(1)).current;
+  const translateX = useRef(new Animated.Value(0)).current; 
+  const translateY = useRef(new Animated.Value(0)).current;
 
-  const translateX = useRef(new Animated.Value(1)).current; 
-  const translateY = useRef(new Animated.Value(1)).current;
-
-  const posX = useRef(new Animated.Value(0)).current;
-  const posY = useRef(new Animated.Value(0)).current;
-
-  // const directionY = posY.interpolate({
-  //     inputRange: [-BOUND_Y, 0, BOUND_Y],
-  //     outputRange: [1, 1, -1],
-  //   })
-  
-  //   const directionX = posX.interpolate({
-  //     inputRange: [-BOUND_X, 0, BOUND_X],
-  //     outputRange: [1, 1, -1],
-  //   });
-    
-  // const translateY = directionY.interpolate({
-  //   inputRange: [-1, 1],
-  //   outputRange: [-BOUND_Y, BOUND_Y],
-  // })
-  
-  // const translateX = directionX.interpolate({
-  //   inputRange: [-1, 1],
-  //   outputRange: [-BOUND_X, BOUND_X],
-  // })
+  const [directionX, setDirectionX] = useState(1)
+  const [directionY, setDirectionY] = useState(1)
 
   useEffect(() => {
-    Animated.parallel(
-      [
-        Animated.spring(translateX, {
-          toValue: directionX.interpolate({
-            inputRange: [-1, 1],
-            outputRange: [-BOUND_X, BOUND_X],
-          }),
-          useNativeDriver: true,
-        }),
-        Animated.spring(translateY, {
-          toValue: directionY.interpolate({
-            inputRange: [-1, 1],
-            outputRange: [-BOUND_Y, BOUND_Y],
-          }),
-          useNativeDriver: true,
-        }),
-        
-        Animated.spring(directionX, {
-          toValue: posX.interpolate({
-            inputRange: [0, BOUND_X],
-            outputRange: [1, -1],
-          }),
-          useNativeDriver: true,
-        }),
-        Animated.spring(directionY, {
-          toValue: posY.interpolate({
-            inputRange: [0, BOUND_Y],
-            outputRange: [1, -1],
-          }),
-          useNativeDriver: true,
-        }),
-      ],
-      {
-        useNativeDriver: true,
-      }
-    ).start();
-
-    translateX.addListener(x=>console.log("transX",x.value))
-    translateY.addListener(x=>console.log("transY",x.value))
-    directionX.addListener(x=>console.log("dirX",x.value))
-    directionY.addListener(x=>console.log("dirY",x.value))
-    posX.addListener(x=>console.log("posX",x.value))
-    posY.addListener(x=>console.log("posY",x.value))
-    
-
+    translateX.addListener(tx=>{
+      if (tx.value >= BOUND_X-5) setDirectionX(-1);
+      else if (tx.value <= 5) setDirectionX(1);
+    })
+    translateY.addListener(ty=>{
+      if (ty.value >= BOUND_Y-5) setDirectionY(-1);
+      else if (ty.value <= 5) setDirectionY(1);
+    })
   },[]);
+
+  useEffect(() => {
+    Animated.timing(translateX, {
+      toValue: BOUND_X * directionX,
+      duration: 5500,
+      timing: Easing.linear,
+      useNativeDriver: true,
+    }).start()
+  }, [directionX])
+
+  useEffect(() => {
+  Animated.timing(translateY, {
+      toValue: BOUND_Y * directionY,
+      duration: 5000,
+      timing: Easing.linear,
+      useNativeDriver: true,
+    }).start()
+  }, [directionY])
 
   return (
     <Animated.View style={styles.container}>
       <Animated.View
-        onLayout={Animated.event(
-          [{ nativeEvent: { layout: { x: posX, y: posY } } }],
-          { useNativeDriver: true }
-        )}
         style={{
           width: IMAGE_WIDTH,
           height: IMAGE_HEIGHT,
           position: "absolute",
-          backgroundColor: "red",
+          top: 0,
+          left: 0,
           transform: [{ translateX }, { translateY }],
         }}
-      >
-        <Animated.Text>
-          Open up App.js to start working on your app!
-        </Animated.Text>
+      ><DvdLogo width={IMAGE_WIDTH} height={IMAGE_HEIGHT} />
       </Animated.View>
     </Animated.View>
   );
@@ -111,7 +66,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
   },
